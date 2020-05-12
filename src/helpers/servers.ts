ad737ced00a523d69d2416aab9aabc7b493dae37
Kay Domrose <kay@domrose.org>
2020-05-12 19:33:45 +0200

    Handle other files and invalid servers in server list

--- src/helpers/servers.ts
+++ src/helpers/servers.ts
@@ -1,7 +1,22 @@
 import { SERVERS_PATH } from '../variables';
 import fs from 'fs';
 import { Server } from '../classes/Server';
+import { logError } from './log';
 
 export const allServers = (): Server[] => {
-    return fs.readdirSync(SERVERS_PATH).map((id) => Server.buildFromId(id));
+    const files = fs.readdirSync(SERVERS_PATH);
+    const serverDirs = files.filter((file) =>
+        fs.lstatSync(`${SERVERS_PATH}/${file}`).isDirectory()
+    );
+
+    const servers = serverDirs.map((id) => {
+        try {
+            return Server.buildFromId(id);
+        } catch (e) {
+            logError(`Cannot find server configuration for "${id}"`);
+            return null;
+        }
+    });
+
+    return servers.filter((s) => s instanceof Server) as Server[];
 };
